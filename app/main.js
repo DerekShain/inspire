ea86294dbbbce05af34bafeb98919dfdb2ae940a
Derek Shain <derek0anthony@gmail.com>
2021-09-10 19:13:16 -0600

    fixing clock

--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+import { ClockController } from "./Controllers/ClockController.js";
 import { ImageController } from "./Controllers/ImageController.js";
 import { QuoteController } from "./Controllers/QuoteController.js";
 
@@ -5,6 +6,7 @@ import { QuoteController } from "./Controllers/QuoteController.js";
 class App {
   imageController = new ImageController()
   quoteController = new QuoteController()
+  clockController = new ClockController()
 }
 
 window["app"] = new App();
